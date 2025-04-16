f7c8c82c37dadb69830a7e01fcb9579364d625e6
Jingyi <jingyi012@gmail.com>
2025-04-17 01:20:34 +0800

    Update submit invoice - check submission, block duplicate submission - map user uom to lhdn uom - store record after getting uuid - return error response from lhdn if fail

--- api/modules/Invoice/Invoice.Application/Features/InvoiceDocuments/SubmitInvoice/v1/SubmitInvoiceCommand.cs
+++ api/modules/Invoice/Invoice.Application/Features/InvoiceDocuments/SubmitInvoice/v1/SubmitInvoiceCommand.cs
@@ -3,4 +3,6 @@ using NexKoala.WebApi.Invoice.Application.Dtos.EInvoice.Invoice;
 
 namespace NexKoala.WebApi.Invoice.Application.Features.InvoiceDocuments.SubmitInvoice.v1;
 
-public partial class SubmitInvoiceCommand : InvoiceRequest, IRequest<object> { }
+public partial class SubmitInvoiceCommand : InvoiceRequest, IRequest<object> { 
+    public string? UserId { get; set; }
+}
